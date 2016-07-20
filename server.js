8f22b7549f2954bba111ef880d2275d53e4d130a
Yury Hurski <hi1sailor@gmail.com>
2016-07-20 18:18:02 +0300

    add package wget

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
 	  response.writeHead(200, {"Content-Type": "text/plain"});
-	  response.write("test");
+	  response.write("Test");
 	  response.end();
 }).listen(process.env.PORT || 8888);
